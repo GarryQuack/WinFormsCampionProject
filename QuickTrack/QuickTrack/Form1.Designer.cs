54e3a092628da513700598e07b27a4b39fa5202f
RianQ <RianQ@LILGERALD>
2026-06-06 06:06:20 +0100

    Fix shit, work on shit, do shit

--- QuickTrack/QuickTrack/Form1.Designer.cs
+++ QuickTrack/QuickTrack/Form1.Designer.cs
@@ -53,19 +53,21 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(63, 250);
+            label2.Location = new Point(90, 417);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(118, 16);
+            label2.Size = new Size(175, 24);
             label2.TabIndex = 2;
             label2.Text = "No Name Entered";
             label2.Visible = false;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(63, 215);
+            textBox1.Location = new Point(90, 358);
+            textBox1.Margin = new Padding(4, 5, 4, 5);
             textBox1.MaxLength = 128;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(387, 23);
+            textBox1.Size = new Size(551, 31);
             textBox1.TabIndex = 3;
             textBox1.WordWrap = false;
             textBox1.TextChanged += textBox1_TextChanged;
@@ -74,19 +76,21 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Roobert", 15F, FontStyle.Bold);
-            label1.Location = new Point(63, 188);
+            label1.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold);
+            label1.Location = new Point(83, 313);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(142, 24);
+            label1.Size = new Size(211, 36);
             label1.TabIndex = 4;
             label1.Text = "Enter a name:";
             // 
             // button1
             // 
             button1.ForeColor = SystemColors.ActiveCaptionText;
-            button1.Location = new Point(468, 215);
+            button1.Location = new Point(669, 358);
+            button1.Margin = new Padding(4, 5, 4, 5);
             button1.Name = "button1";
-            button1.Size = new Size(78, 25);
+            button1.Size = new Size(111, 42);
             button1.TabIndex = 5;
             button1.Text = "Add Name";
             button1.UseVisualStyleBackColor = true;
@@ -95,9 +99,10 @@
             // button2
             // 
             button2.ForeColor = SystemColors.ActiveCaptionText;
-            button2.Location = new Point(562, 215);
+            button2.Location = new Point(803, 358);
+            button2.Margin = new Padding(4, 5, 4, 5);
             button2.Name = "button2";
-            button2.Size = new Size(78, 25);
+            button2.Size = new Size(111, 42);
             button2.TabIndex = 6;
             button2.Text = "Print All";
             button2.UseVisualStyleBackColor = true;
@@ -106,20 +111,22 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Roobert", 27.7499962F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.Location = new Point(63, 124);
+            label3.Font = new Font("Microsoft Sans Serif", 27.7499962F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(75, 207);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(224, 44);
+            label3.Size = new Size(341, 64);
             label3.TabIndex = 7;
             label3.Text = "Start of Run:";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Roobert", 27.7499962F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.Location = new Point(63, 323);
+            label4.Font = new Font("Microsoft Sans Serif", 27.7499962F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(75, 538);
+            label4.Margin = new Padding(4, 0, 4, 0);
             label4.Name = "label4";
-            label4.Size = new Size(210, 44);
+            label4.Size = new Size(321, 64);
             label4.TabIndex = 8;
             label4.Text = "End of Run:";
             // 
@@ -127,9 +134,10 @@
             // 
             FinishRunner.Font = new Font("Segoe UI", 9F);
             FinishRunner.ForeColor = SystemColors.ActiveCaptionText;
-            FinishRunner.Location = new Point(468, 423);
+            FinishRunner.Location = new Point(669, 705);
+            FinishRunner.Margin = new Padding(4, 5, 4, 5);
             FinishRunner.Name = "FinishRunner";
-            FinishRunner.Size = new Size(78, 25);
+            FinishRunner.Size = new Size(111, 42);
             FinishRunner.TabIndex = 13;
             FinishRunner.Text = "End Runner";
             FinishRunner.UseVisualStyleBackColor = true;
@@ -138,19 +146,21 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Roobert", 15F, FontStyle.Bold);
-            label5.Location = new Point(63, 396);
+            label5.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold);
+            label5.Location = new Point(83, 660);
+            label5.Margin = new Padding(4, 0, 4, 0);
             label5.Name = "label5";
-            label5.Size = new Size(142, 24);
+            label5.Size = new Size(211, 36);
             label5.TabIndex = 12;
             label5.Text = "Enter a name:";
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(63, 423);
+            textBox2.Location = new Point(90, 705);
+            textBox2.Margin = new Padding(4, 5, 4, 5);
             textBox2.MaxLength = 128;
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(387, 23);
+            textBox2.Size = new Size(551, 31);
             textBox2.TabIndex = 11;
             textBox2.WordWrap = false;
             // 
@@ -158,9 +168,10 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label6.Location = new Point(63, 458);
+            label6.Location = new Point(90, 763);
+            label6.Margin = new Padding(4, 0, 4, 0);
             label6.Name = "label6";
-            label6.Size = new Size(118, 16);
+            label6.Size = new Size(175, 24);
             label6.TabIndex = 10;
             label6.Text = "No Name Entered";
             label6.Visible = false;
@@ -168,9 +179,10 @@
             // ButtonFinishRace
             // 
             ButtonFinishRace.ForeColor = SystemColors.ActiveCaptionText;
-            ButtonFinishRace.Location = new Point(562, 423);
+            ButtonFinishRace.Location = new Point(803, 705);
+            ButtonFinishRace.Margin = new Padding(4, 5, 4, 5);
             ButtonFinishRace.Name = "ButtonFinishRace";
-            ButtonFinishRace.Size = new Size(78, 25);
+            ButtonFinishRace.Size = new Size(111, 42);
             ButtonFinishRace.TabIndex = 14;
             ButtonFinishRace.Text = "Finish Race";
             ButtonFinishRace.UseVisualStyleBackColor = true;
@@ -179,10 +191,11 @@
             // TitleLabel
             // 
             TitleLabel.AutoSize = true;
-            TitleLabel.Font = new Font("Roobert", 47.9999924F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
-            TitleLabel.Location = new Point(12, 9);
+            TitleLabel.Font = new Font("Roobert-TRIAL SemiBold", 47.9999962F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
+            TitleLabel.Location = new Point(17, 15);
+            TitleLabel.Margin = new Padding(4, 0, 4, 0);
             TitleLabel.Name = "TitleLabel";
-            TitleLabel.Size = new Size(372, 77);
+            TitleLabel.Size = new Size(549, 134);
             TitleLabel.TabIndex = 15;
             TitleLabel.Text = "QuickTrack";
             // 
@@ -192,10 +205,10 @@
             TimeKeeping.BorderStyle = BorderStyle.FixedSingle;
             TimeKeeping.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             TimeKeeping.ForeColor = Color.FromArgb(192, 0, 0);
-            TimeKeeping.Location = new Point(758, 26);
+            TimeKeeping.Location = new Point(1083, 43);
             TimeKeeping.Margin = new Padding(0);
             TimeKeeping.Name = "TimeKeeping";
-            TimeKeeping.Size = new Size(450, 60);
+            TimeKeeping.Size = new Size(642, 99);
             TimeKeeping.TabIndex = 16;
             TimeKeeping.Text = "Race has not started yet!";
             TimeKeeping.TextAlign = ContentAlignment.MiddleCenter;
@@ -204,9 +217,10 @@
             // 
             RaceStart.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             RaceStart.ForeColor = SystemColors.ActiveCaptionText;
-            RaceStart.Location = new Point(858, 158);
+            RaceStart.Location = new Point(1226, 263);
+            RaceStart.Margin = new Padding(4, 5, 4, 5);
             RaceStart.Name = "RaceStart";
-            RaceStart.Size = new Size(257, 114);
+            RaceStart.Size = new Size(367, 190);
             RaceStart.TabIndex = 17;
             RaceStart.Text = "Start Race!";
             RaceStart.UseVisualStyleBackColor = true;
@@ -220,19 +234,20 @@
             // Time
             // 
             Time.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Time.Location = new Point(940, 110);
+            Time.Location = new Point(1343, 183);
+            Time.Margin = new Padding(4, 0, 4, 0);
             Time.Name = "Time";
-            Time.Size = new Size(98, 20);
+            Time.Size = new Size(140, 33);
             Time.TabIndex = 18;
             Time.Text = "00:00:00.0000";
             Time.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(108, 20, 21);
-            ClientSize = new Size(1264, 681);
+            ClientSize = new Size(1806, 1135);
             Controls.Add(Time);
             Controls.Add(RaceStart);
             Controls.Add(TimeKeeping);
@@ -251,6 +266,7 @@
             Controls.Add(label2);
             ForeColor = SystemColors.Control;
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 5, 4, 5);
             Name = "Form1";
             Text = "QuickTrack";
             Load += Form1_Load;
